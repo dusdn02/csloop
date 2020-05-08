7bcd055a05b2d6ef0fb03f7af4a8989ae3339715
연우 이 <lyw9114@gmail.com>
2020-05-08 12:26:22 +0900

    지렁이

--- csloop/Program.cs
+++ csloop/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace csloop
 {
@@ -10,31 +11,44 @@ namespace csloop
     {
         static void Main(string[] args)
         {
-            long start = DateTime.Now.Ticks;
-            long count = 0;
-            while(start + (10000000)> DateTime.Now.Ticks)
+            //long start = DateTime.Now.Ticks;
+            //long count = 0;
+            //while(start + (10000000)> DateTime.Now.Ticks)
+            //{
+            //    count++;
+            //}
+
+            //Console.WriteLine(count + "만큼 반복함");
+
+
+
+            //string[] array = { "사과", "배", "포도", "딸기", "바나나" };
+            //foreach (string item in array)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //foreach (var item in array)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            int x = 1;
+            while (x < 50)
             {
-                count++;
+                Console.Clear();
+                Console.SetCursorPosition(x, 5);
+                if (x % 3 == 0)
+                    Console.WriteLine("__@");
+                else if (x % 3 == 1)
+                    Console.WriteLine("_^@");
+                else
+                    Console.WriteLine("^_@");
+                Thread.Sleep(100);
+                x++;
+
+
             }
-
-            Console.WriteLine(count + "만큼 반복함");
-
-
-
-            string[] array = { "사과", "배", "포도", "딸기", "바나나" };
-            foreach (string item in array)
-            {
-                Console.WriteLine(item);
-            }
-
-            foreach (var item in array)
-            {
-                Console.WriteLine(item);
-            }
-
-
-
         }
-
     }
 }
